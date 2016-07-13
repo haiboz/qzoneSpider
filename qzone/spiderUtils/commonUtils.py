d017d0b2019f25858d1a0756adc4beac121cbd09
haiboz <798102408@qq.com>
2016-07-13 09:23:12 +0800

    公共方法添加

---
+++ qzone/spiderUtils/commonUtils.py
@@ -0,0 +1,23 @@
+# coding:utf8
+'''
+Created on 2016-7-12
+
+@author: 浮生若梦
+'''
+import time
+
+class CommonUtils(object):
+    
+    def getLocalTime(self):
+        '''获取当地格式化后的时间'''
+        ISOTIMEFORMAT="%Y-%m-%d %X"
+        localtime = time.strftime( ISOTIMEFORMAT, time.localtime() )
+        return localtime
+    
+    def waitWebLoad(self,browser):
+        '''判断页面是否加载完成'''
+        
+#         browser.
+        pass
+
+
